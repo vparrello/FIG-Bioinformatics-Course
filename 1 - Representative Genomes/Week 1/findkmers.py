c7884971cab1f84e2a1b79352efa16c0899b3954
vparrello <parrellovictoria@gmail.com>
2024-02-12 09:18:07 -0600

    adjust worksheet

--- 1 - Representative Genomes/Week 1/findkmers.py
+++ 1 - Representative Genomes/Week 1/findkmers.py
@@ -7,7 +7,7 @@ Activity 1: Find the secret sequences
 dna_sequences = ["a", "g", "c", "t"]
 header = ""
 
-with open("Data/Sample1.fasta") as datafile:
+with open("1 - Representative Genomes/Week 1/Data/Sample1.fasta") as datafile:
     data = #TODO put something about *readlines* in here
     for line in data:
         if "TODO insert header pattern here" in line:
