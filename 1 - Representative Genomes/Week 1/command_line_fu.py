cc4bf0068970e3fb9952669c1638060439d19406
Tory <parrellovictoria@gmail.com>
2024-03-10 16:06:11 -0500

    Exercise 2

---
+++ 1 - Representative Genomes/Week 1/command_line_fu.py
@@ -0,0 +1,14 @@
+################################################
+#           Insert your Psuedo Code Below      #
+# Use ### before your text to ensure it does   #
+# not register as code. The three quotes below #
+# will protect you                             #
+"""
+Psuedo Code Goes Here:
+
+    
+"""
+################################################
+
+import real_code from Grimoire
+pass
