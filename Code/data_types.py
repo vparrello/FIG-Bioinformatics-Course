1a1235deae9ce05848966dfeec94617fb4e5301d
vparrello <parrellovictoria@gmail.com>
2024-06-17 16:57:30 -0500

    fix datatypes code

--- Code/data_types.py
+++ Code/data_types.py
@@ -6,7 +6,7 @@ integer = 158
 integer = 15
 string = "strings are any combination of characters"
 list = []
-set = set()
+_set = set()
 tuple = tuple()
 dictionary = {}
 
@@ -16,33 +16,27 @@ dictionary = {}
 # Open a datafile to parse into the different datatypes. Only those without default values are populated.
 with open("Data/rep200.list.tbl") as data_file: # type: ignore
     # Read the data file. Changes the "File" datatype into a "List" datatype
-    list = data_file.readlines()
     data = data_file.readlines()
     # Look at the lines one by one through the list. This is looking at each string in the list.
-    for line in list:
-        for line in data:
-            # Take the new line character off the end of the string
-            string = line.strip("\n")
-            line = line.strip("\n")
-            # Split the string into columns by separating by the Tab character. This changes the datatype into a list again.
-            line = line.split("\t")
-            list = line.split("\t")
-            list.append("Foo")
-            list.append("315")
-            list.append("1214")
-            # Look for every line except the header line. This keeps meta data out of our data.
-            set = line
-            _set = set(list)
-            if "genome_id" not in line:
-                # Make a key value pair by adding the genome_id at index 0 as the key and the genome_name at index 1 as the value. 
-                # Example: {'511145.12': 'Escherichia coli str. K-12 substr. MG1655'}
-                dictionary[line[0]] = line[1]
-                tuple = (line[0],line[1])
-                dictionary[list[0]] = list[1]
-                tuple = (list[0],list[1])
-                integer += 1
-                if integer > 40:
-                    break
+    for line in data:
+        # Take the new line character off the end of the string
+        string = line.strip("\n")
+        line = line.strip("\n")
+        # Split the string into columns by separating by the Tab character. This changes the datatype into a list again.
+        list = line.split("\t")
+        list.append("Foo")
+        list.append("315")
+        list.append("1214")
+        # Look for every line except the header line. This keeps meta data out of our data.
+        _set = set(list)
+        if "genome_id" not in line:
+            # Make a key value pair by adding the genome_id at index 0 as the key and the genome_name at index 1 as the value. 
+            # Example: {'511145.12': 'Escherichia coli str. K-12 substr. MG1655'}
+            dictionary[list[0]] = list[1]
+            tuple = (list[0],list[1])
+            integer += 1
+            if integer > 40:
+                break
 
     # Check if the datatype is provided as a command-line argument
     if len(sys.argv) != 2:
@@ -59,19 +53,14 @@ with open("Data/rep200.list.tbl") as data_file: # type: ignore
     #Booleans
     if datatype == "boolean":
         print(boolean)
-    #Integers
+    elif datatype == "string":
+        print(string)
     elif datatype == "integer":
         print(integer)
-    # Sets
     elif datatype == "set":
-        print(set)
         print(_set)
-    # Tuples
-    elif datatype == "tuples":
-        print(dictionary)
     elif datatype == "tuple":
         print(tuple)
-    # Dictionaries
     elif datatype == "dictionary":
         print(dictionary)
     else:
